5612cd107c4f115ef001164fba32dd554379255c
Nawaz Haider <nawazhaider60@gmail.com>
2025-01-02 21:35:05 +0600

    Add cryptocurrency analysis agents and update main functionality

---
+++ agents.py
@@ -0,0 +1,42 @@
+from phi.agent import Agent
+from phi.model.groq import Groq
+from phi.tools.duckduckgo import DuckDuckGo
+from phi.tools.googlesearch import GoogleSearch
+from phi.tools.newspaper4k import Newspaper4k
+from phi.tools.yfinance import YFinanceTools
+from dotenv import load_dotenv
+
+load_dotenv()
+
+web_searcher = Agent(
+    model=Groq(id="llama-3.3-70b-versatile"),
+    name="Web Searcher",
+    role="Searches the web for information on a topic",
+    tools=[DuckDuckGo()],
+    markdown=True,
+    # add_datetime_to_instructions=True,
+)
+
+article_reader = Agent(
+    model=Groq(id="llama-3.3-70b-versatile"),
+    name="Article Reader",
+    role="Reads articles from URLs.",
+    tools=[Newspaper4k()],
+    markdown=True,
+)
+
+yahoo_finance_agent = Agent(
+    name="Yahoo Finance Agent",
+    model=Groq(id="llama-3.3-70b-versatile"),
+    tools=[
+        YFinanceTools(
+            stock_price=True,
+            analyst_recommendations=True,
+            company_info=True,
+            company_news=True,
+        )
+    ],
+    instructions=["Use tables to display data"],
+    show_tool_calls=True,
+    markdown=True,
+)
--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 from phi.agent import Agent
 from phi.model.groq import Groq
+from agents import web_searcher, article_reader, yahoo_finance_agent
 from dotenv import load_dotenv
 
 load_dotenv()
 
 agent = Agent(
-    model=Groq(id="llama-3.3-70b-versatile")
+    model=Groq(id="llama-3.3-70b-versatile"),
+    name="Cryptocurrency Analyst",
+    team=[web_searcher, article_reader, yahoo_finance_agent],
+    instructions=[
+        "1. Use the web searcher to find the latest news.",
+        "2. Provide the article reader with the top links from the search results to gather important information.",
+        "3. Ensure the article reader reviews the links to extract key details.",
+        "4. Use the Yahoo Finance agent to retrieve detailed information and analysis.",
+        "5. Provide a summary of the analyst recommendations, including quantitative data in a table.",
+        "6. Offer investment advice based on the gathered information, presented in a table.",
+        "7. Provide both bullish and bearish signals",
+    ],
+    # reasoning=True,
+    debug_mode=True,
+    show_tool_calls=True,
+    markdown=True,
 )
 
-agent.print_response("Say something about Bitcoin and Ethereum")
+agent.print_response("Provide reccomendation for BTC", stream=True)
